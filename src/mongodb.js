4c10add359196b2ce6ee44f989b7a258dac6447a
André Ramos <andremilani34@gmail.com>
2025-10-25 23:29:35 -0300

    Melhorando mensagens do console

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,18 +1,21 @@
-import "dotenv/config";
 import mongoose from "mongoose";
+import chalk from "chalk";
+import "dotenv/config";
 
 export async function ConnectMongo() {
   const uri = `${process.env.MONGO_URL}${process.env.MONGO_DATABASE_NAME}`;
 
   //   Verifica se a URI realmente existe
   if (!uri) {
-    return console.error("Sua URI não existe, verifique seu mongodb.js");
+    return console.error(
+      chalk.red("Sua URI não existe, verifique seu mongodb.js")
+    );
   }
 
   try {
     await mongoose.connect(uri);
-    console.log("Conectado ao MongoDB");
+    console.log(chalk.green("Conectado ao MongoDB \n"));
   } catch (error) {
-    console.error("Erro ao se conectar com mongo:", error);
+    console.error(chalk.red("Erro ao se conectar com mongo:", error));
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import { ConnectMongo } from "./mongodb.js";
 import { app } from "./app.js";
+import chalk from "chalk";
 import "dotenv/config";
 
 function Server() {
   app.listen({ host: process.env.HOST, port: process.env.PORT }).then(() => {
-    console.log(`API on em ${process.env.HOST}:${process.env.PORT}`);
+    console.log(
+      chalk.green(`API on em ${process.env.HOST}:${process.env.PORT}`)
+    );
   });
 }
 
